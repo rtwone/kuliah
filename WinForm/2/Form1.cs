c8d5ff73c63fff6de92d6e4673016d361d3b7a80
Irfan Hariyanto <78160909+rtwone@users.noreply.github.com>
2025-12-15 21:12:11 +0700

    Add files via upload

---
+++ WinForm/2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            labels.Text = labels.Text == "label1" ? "Anjay" : "label1";
+        }
+
+        private void labels_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Tes";
+        }
+
+        private void labels_MouseEnter(object sender, EventArgs e)
+        {
+            labels.Text = "Tesss";
+        }
+    }
+}
